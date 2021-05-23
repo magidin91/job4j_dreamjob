0b959f30cedac9cb9596718bd1f40540135a1149
magidin91 <magidin@mail.ru>
2021-05-23 20:02:07 +0500

    4. candicates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/job4j/dream/model/Post.java
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -12,6 +12,11 @@ public class Post {
     private String description;
     private Date created;
 
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Post(int id, String name, String description, Date created) {
         this.id = id;
         this.name = name;
